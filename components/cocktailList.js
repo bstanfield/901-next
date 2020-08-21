6afbbefc10ac17e6803586860d5f0db11314996b
Ben Stanfield <bnstnfld@gmail.com>
2020-08-21 00:49:23 -0700

    cut more code + special message for no cocktails

--- components/cocktailList.js
+++ components/cocktailList.js
@@ -1,8 +1,8 @@
 import Cocktail from './cocktail'
 
 export default function CocktailList({ cocktails, filters, displayMaximum }) {
-  if (cocktails == []) {
-    return (null);
+  if (cocktails.length === 0) {
+    return (<div className="sadCat">🙈</div>);
   }
 
   let cocktailsToDisplay = cocktails.slice(0, displayMaximum || 901)
