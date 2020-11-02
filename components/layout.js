ec494ee5f3e4428909b9c19348812b13d46d8c77
Ben Stanfield <bnstnfld@gmail.com>
2020-11-01 17:19:41 -0800

    tippy working

--- components/layout.js
+++ components/layout.js
@@ -1,39 +1,44 @@
 import Head from 'next/head'
+import { Fragment } from 'react'
+import TipsButton from './tipsButton'
 
 export const siteTitle = '901 Cocktails'
 
 export default function Layout({ children, home }) {
   return (
-    <div className="container">
-      <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,700;1,700&display=swap" rel="stylesheet" />
-        <meta
-          name="description"
-          content="Search for the very best cocktails you can make with ingredients on hand."
-        />
-        <meta
-          property="og:image"
-          content="https://i.imgur.com/PA7puwU.png"
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <header className="header">
-        {home && (
-          <div className="leader">
-            <h1 className="heading2Xl"><i>901 Cocktails</i></h1>
-            <section className="intro">
-              <p>
-                Recipes from the book <a target="_blank" rel="noopener" href="http://www.901cocktails.com">901 Cocktails</a>
-              </p>
-              <p>Website by <a href="https://benstanfield.io">Ben Stanfield</a></p>
-            </section>
-          </div>
-        )
-        }
-      </header >
-      <main>{children}</main>
-      <div className="scrollToTop" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>▲ Scroll to top</div>
-    </div >
+    <Fragment>
+      <TipsButton />
+      <div className="container">
+        <Head>
+          <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,700;1,700&display=swap" rel="stylesheet" />
+          <meta
+            name="description"
+            content="Search for the very best cocktails you can make with ingredients on hand."
+          />
+          <meta
+            property="og:image"
+            content="https://i.imgur.com/PA7puwU.png"
+          />
+          <meta name="og:title" content={siteTitle} />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Head>
+        <header className="header">
+          {home && (
+            <div className="leader">
+              <h1 className="heading2Xl"><i>901 Cocktails</i></h1>
+              <section className="intro">
+                <p>
+                  Recipes from the book <a target="_blank" rel="noopener" href="http://www.901cocktails.com">901 Cocktails</a>
+                </p>
+                <p>Website by <a href="https://benstanfield.io">Ben Stanfield</a></p>
+              </section>
+            </div>
+          )
+          }
+        </header >
+        <main>{children}</main>
+        <div className="scrollToTop" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>▲ Scroll to top</div>
+      </div >
+    </Fragment>
   )
 }
---
+++ components/tipsButton.js
@@ -0,0 +1,26 @@
+import {
+  Tooltip,
+} from 'react-tippy';
+
+export default function tipsButton() {
+  return (
+
+    <div className="circle">
+      <Tooltip
+        // options
+        title="View tips & tricks"
+        interactive={true}
+        html={<a className='tooltipText' href='/tips'>View tips & tricks</a>}
+        position="left"
+        trigger="mouseenter"
+        distance={16}
+        offset={-16}
+        animation="fade"
+        animateFill={false}
+      >
+        <div className="questionMark">?</div>
+      </Tooltip >
+    </div >
+
+  )
+}
